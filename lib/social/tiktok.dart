0898064de3470a6293a71b080b1c2a97ffe6be2f
dola <mohamed.adeltawfiq@roaa.tech>
2024-12-28 23:13:17 +0200

    add readme "need to refactor"

--- lib/social/tiktok.dart
+++ lib/social/tiktok.dart
@@ -5,9 +5,9 @@ import 'package:flutter/services.dart';
 class Tiktok {
   static const platform = MethodChannel('social_sharing');
 
-  static share(List<String> file) async {
+  static share(List<String> files) async {
     try {
-      await platform.invokeMethod('shareToTiktok', {'filePaths': file});
+      await platform.invokeMethod('shareToTiktok', {'filePaths': files});
     } on PlatformException catch (e, s) {
       log("Failed with error : '${e.message}'. \n $s");
       throw (e, s);
