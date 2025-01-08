f13462eca624a6d55aea4d6c2b52ae5cd4f631b1
dola <mohamed.adeltawfiq@roaa.tech>
2025-01-08 02:38:13 +0200

    return share to android function

--- lib/social/tiktok.dart
+++ lib/social/tiktok.dart
@@ -5,7 +5,17 @@ import 'package:flutter/services.dart';
 class Tiktok {
   static const platform = MethodChannel('social_sharing');
 
-
+  static shareToAndroid(List<String> files) async {
+    try {
+      await platform.invokeMethod('shareToTiktok', {'filePaths': files});
+    } on PlatformException catch (e, s) {
+      log("Failed with error : '${e.message}'. \n $s");
+      throw (e, s);
+    } catch (e, s) {
+      log("Failed with error : '$e'. \n $s");
+      throw (e, s);
+    }
+  }
   static shareToTikTokMultiFiles1(
       {required List<String> files,
       required String redirectUrl,
